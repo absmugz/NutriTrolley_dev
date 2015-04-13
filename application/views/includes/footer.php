b10f44d2b88400193e62170d5c50e8dd3808b2e4
absmugz <absmugz09@gmail.com>
2015-04-13 09:57:24 +0200

    Include bootstrap

---
+++ application/views/includes/footer.php
@@ -0,0 +1,4 @@
+</body>
+<script type="text/javascript" src="<?php echo base_url("public/js/jquery.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("public/js/bootstrap.js"); ?>"></script>
+</html>
---
+++ application/views/includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<title></title>
+<link rel="stylesheet" href="<?php echo base_url("public/css/bootstrap.css"); ?>">
+<link rel="stylesheet" href="<?php echo base_url("public/css/style.css"); ?>">
+
+
+<body>
+
+
+
