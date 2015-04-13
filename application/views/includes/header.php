2869794bd44ff427c2bb22b3079809608bf99732
absmugz <absmugz09@gmail.com>
2015-04-13 10:49:33 +0200

    Include fancy box lightbox
    configuring  fancybox Lightbox for codeigniter

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,6 +5,14 @@
 	<title></title>
 <link rel="stylesheet" href="<?php echo base_url("public/css/bootstrap.css"); ?>">
 <link rel="stylesheet" href="<?php echo base_url("public/css/style.css"); ?>">
+<!-- Add fancyBox -->
+
+<link rel="stylesheet" href="<?php echo base_url("public/fancybox/source/jquery.fancybox.css?v=2.1.5"); ?>" type="text/css" media="screen" />
+<!-- Optionally add helpers - button, thumbnail and/or media -->
+<link rel="stylesheet" href="<?php echo base_url("public/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5"); ?>" type="text/css" media="screen" />
+
+<link rel="stylesheet" href="<?php echo base_url("public/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7"); ?>" type="text/css" media="screen" />
+
 
 
 <body>
--- application/views/main_content.php
+++ application/views/main_content.php
@@ -1,1 +1,8 @@
-main content goes here.
+<ul class="list">
+	<li>
+		<a class="various fancybox.iframe" href="http://www.youtube.com/embed/L9szn1QQfas?autoplay=1">Youtube (iframe)</a>
+	</li>
+	<li>
+		<a class="various fancybox.iframe" href="http://maps.google.com/?output=embed&f=q&source=s_q&hl=en&geocode=&q=London+Eye,+County+Hall,+Westminster+Bridge+Road,+London,+United+Kingdom&hl=lv&ll=51.504155,-0.117749&spn=0.00571,0.016512&sll=56.879635,24.603189&sspn=10.280244,33.815918&vpsrc=6&hq=London+Eye&radius=15000&t=h&z=17">Google maps (iframe)</a>
+	</li>
+</ul>
